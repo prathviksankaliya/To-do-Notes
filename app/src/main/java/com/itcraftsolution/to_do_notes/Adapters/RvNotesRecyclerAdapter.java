a953d8638a1859c0df63f7a64349827cdef1423b
Shadow <prathviksankaliya@gmail.com>
2023-07-23 18:51:53 +0530

    CRUD Operation Successfully Done

--- app/src/main/java/com/itcraftsolution/to_do_notes/Adapters/RvNotesRecyclerAdapter.java
+++ app/src/main/java/com/itcraftsolution/to_do_notes/Adapters/RvNotesRecyclerAdapter.java
@@ -8,11 +8,17 @@ import android.os.ProxyFileDescriptorCallback;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.snackbar.Snackbar;
 import com.itcraftsolution.to_do_notes.Activities.AddNotesActivity;
 import com.itcraftsolution.to_do_notes.Fragments.AddNotesFragment;
 import com.itcraftsolution.to_do_notes.Models.Notes;
@@ -52,6 +58,79 @@ public class RvNotesRecyclerAdapter extends RecyclerView.Adapter<RvNotesRecycler
             public void onClick(View v) {
                 PrefConfig.updateNotes(context, notes.getId(), holder.getAdapterPosition(), notes.getTitle(), notes.getDesc(), notes.getDate(), true);
                 context.startActivity(new Intent(context, AddNotesActivity.class));
+            }
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                BottomSheetDialog menuBottomSheetDialog = new BottomSheetDialog(context, R.style.BottomSheetStyle);
+                View menuSheetView = LayoutInflater.from(context).inflate(R.layout.note_option, view.findViewById(R.id.bottomSheetMenu));
+                menuBottomSheetDialog.setContentView(menuSheetView);
+                menuBottomSheetDialog.show();
+                ImageView igEdit, igDelete, igShare;
+
+                igEdit = menuSheetView.findViewById(R.id.btnEdit);
+                igDelete = menuSheetView.findViewById(R.id.btnMenuDelete);
+                igShare = menuSheetView.findViewById(R.id.btnShare);
+
+                igEdit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        PrefConfig.updateNotes(context, notes.getId(), holder.getAdapterPosition(), notes.getTitle(), notes.getDesc(), notes.getDate(), true);
+                        context.startActivity(new Intent(context, AddNotesActivity.class));
+                        menuBottomSheetDialog.dismiss();
+                    }
+                });
+
+                igDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context, R.style.BottomSheetStyle);
+                        View sheetView = LayoutInflater.from(context).inflate(R.layout.delete_box, view.findViewById(R.id.bottomSheetDelete));
+                        bottomSheetDialog.setContentView(sheetView);
+                        bottomSheetDialog.show();
+                        Button canclebtn, deletebtn;
+                        canclebtn = sheetView.findViewById(R.id.btnCancle);
+                        deletebtn = sheetView.findViewById(R.id.btnDelete);
+
+                        deletebtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                list.remove(holder.getAdapterPosition());
+                                PrefConfig.writeNotesInSpf(context, list);
+                                notifyDataSetChanged();
+                                bottomSheetDialog.dismiss();
+                                Snackbar.make(holder.binding.getRoot(), "Deleted Successfully!!", Snackbar.LENGTH_SHORT)
+                                        .setBackgroundTint(context.getResources().getColor(R.color.red))
+                                        .setTextColor(context.getResources().getColor(R.color.white))
+                                        .show();
+                            }
+                        });
+                        canclebtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                bottomSheetDialog.dismiss();
+                            }
+                        });
+                        menuBottomSheetDialog.dismiss();
+                    }
+                });
+
+                igShare.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_SUBJECT, notes.getTitle());
+                        intent.putExtra(Intent.EXTRA_TEXT, notes.getDesc());
+                        context.startActivity(Intent.createChooser(intent, "Share Notes!!"));
+                        menuBottomSheetDialog.dismiss();
+                    }
+                });
+
+                return true;
             }
         });
     }
