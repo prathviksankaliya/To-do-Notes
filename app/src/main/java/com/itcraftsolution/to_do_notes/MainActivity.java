de7bf9b30c851f9289da0ebe1892bbf163012166
Shadow <prathviksankaliya@gmail.com>
2023-07-23 10:38:48 +0530

    Project init

---
+++ app/src/main/java/com/itcraftsolution/to_do_notes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.itcraftsolution.to_do_notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
